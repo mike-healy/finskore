dfb8a97e9b2dd6da665035db8d4dd48ca602e394
Mike Healy <design@mikehealy.com.au>
2018-06-13 20:48:50 +1000

    Started basic scoring logic and display (temporary styling)

--- js/app.js
+++ js/app.js
@@ -5,9 +5,54 @@ const finskore = new Vue({
 
     data: {
         title: 'Finskore',
-
         theme: 'default',
-        
-    }
+
+        //Config
+        playToScore: 50,
+        players: [],
+
+        //State
+        setupGame: false,
+        newPlayer: '',
+    },
     
+    methods: {
+        createNewGame() {
+            this.setupGame = true;
+            console.log('creating game');
+        },
+
+        startGame() {
+            this.setupGame = false;
+            console.log('starting game');
+        },
+
+        addPlayer() {
+            console.log('adding player');
+            this.players.push({
+                name: this.newPlayer,
+                score: 0,
+                strikes: 0
+            });
+
+            this.newPlayer = '';
+        },
+
+       hasStruckOut(player) {
+           return player.strikes >= 3;
+       },
+
+       showStrikes(player) {
+           let str = '';
+           if(player.strikes === 0) {
+               return str;
+           }
+
+           for(let i=0; i<player.strikes; i++) {
+               str += 'x ';
+           }
+           return str;
+       }
+    }
+
 });
